1251d063056d415f3c9620b50cdfccae27d2fd66
Andrew Maczugowski <amaczugowski@gmail.com>
2016-06-28 17:29:57 -0400

    pulling in changes

--- app/models/project.js
+++ app/models/project.js
@@ -2,7 +2,31 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ProjectSchema = new Schema({
-    title: String
+    title: String,
+    generated: Boolean,
+    discipline: [String],
+    summary: String,
+    team: String,
+    image: {type: String, default: null},
+    savings: Number,
+    hours: Number,
+    day: Number,
+    month: Number,
+    year: Number,
+    manager: {
+        name: String,
+        unit: String,
+        function: String,
+        department: String,
+        phone: String,
+        email: String
+    },
+    result: [{
+        summary: String,
+        details: String,
+        savings: Number,
+        hours: Number
+    }]
 });
 
 module.exports = mongoose.model('Project', ProjectSchema);
