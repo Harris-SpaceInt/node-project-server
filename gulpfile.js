71cf7f4254dfa9ae1909d0244f8100e7d640f7bc
Andrew Maczugowski <amaczugowski@gmail.com>
2016-07-14 10:33:37 -0400

    merged

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,7 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 
 gulp.task('watch', ['browserSync'], function() {
-    gulp.watch('public/views/**/*.html', browserSync.reload);
-    gulp.watch('public/js/**/*.js', browserSync.reload);
+    gulp.watch('public/**/*', browserSync.reload);
 });
 
 gulp.task('browserSync', function() {
